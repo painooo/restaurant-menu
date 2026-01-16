62916d9fcfc9a7c6eca69dd92d7cef280afad55a
pyr <jzhangnv@gmail.com>
2026-01-15 23:12:38 -0500

    Added dummy data & quick styling

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import './styles.css'
+import {PageFlip} from 'page-flip';
 
-document.body.textContent = "deadbeef";
+const pageFlip = new PageFlip(document.getElementById('book'),
+    {
+        width: 400, // required parameter - base page width
+        height: 600,  // required parameter - base page height
+        showCover: true
+    }
+);
+
+pageFlip.loadFromHTML(document.querySelectorAll('.item'));
