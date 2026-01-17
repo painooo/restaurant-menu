752e5696a8ed1f7c439c75ba31583a863b558418
pyr <jzhangnv@gmail.com>
2026-01-16 20:01:20 -0500

    Added structure to food items

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ const pageFlip = new PageFlip(document.getElementById('book'),
     {
         width: 400, // required parameter - base page width
         height: 600,  // required parameter - base page height
-        showCover: true
+        showCover: true,
+        mobileScrollSupport: false
     }
 );
 
