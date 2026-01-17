886e6986058b0ee2b22499a947c7412d3cb63716
pyr <jzhangnv@gmail.com>
2026-01-16 21:31:15 -0500

    Changed height to fit whole screen

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import {PageFlip} from 'page-flip';
 const pageFlip = new PageFlip(document.getElementById('book'),
     {
         width: 400, // required parameter - base page width
-        height: 600,  // required parameter - base page height
+        height: 900,  // required parameter - base page height
         showCover: true,
         mobileScrollSupport: false
     }
