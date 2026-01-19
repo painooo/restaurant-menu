6221f95cc20f0b6a6cb9134fc477b57190aeca2b
pyr <jzhangnv@gmail.com>
2026-01-19 14:20:45 -0500

    Fixxed issue where other won't all be on screen

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import {PageFlip} from 'page-flip';
 const pageFlip = new PageFlip(document.getElementById('book'),
     {
         width: 400, // required parameter - base page width
-        height: 900,  // required parameter - base page height
+        height: 700,  // required parameter - base page height
         showCover: true,
         mobileScrollSupport: false
     }
